5131c8af931823ede4f9818d59fa72e25a1cadb6
Brent Haas <brenthaas@gmail.com>
2012-05-11 09:11:25 -0700

    Throwing this up on Heroku

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,17 +2,15 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "Home Page" do
+    before { visit root_path }
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_content('This is a sample app!')
+      page.should have_selector('p', text: 'This is a sample app!')
     end
     it "should have the base title..." do
-      visit '/static_pages/home'
       page.should have_selector('title', 
               text: "Ruby on Rails Tutorial Sample App")
     end
     it "... but not the page title" do
-      visit '/static_pages/home/'
       page.should_not have_selector('title',
               text: "| Home")
     end
@@ -20,14 +18,27 @@ describe "StaticPages" do
   
   describe "About Page" do
     it "Should say 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About Us')
     end
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title', 
               text: "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
   
+  describe "Contact page" do
+  
+      it "should have the h1 'Contact'" do
+        visit contact_path
+        page.should have_selector('h1', text: 'Contact')
+      end
+  
+      it "should have the title 'Contact'" do
+        visit contact_path
+        page.should have_selector('title',
+                      text: "Ruby on Rails Tutorial Sample App | Contact")
+      end
+  end
 end
